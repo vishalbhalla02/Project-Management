10e9a599c2035b2ee64371bcc312003234d414f6
Vishal Bhalla <vishalbhalla722@gmail.com>
2026-03-11 11:45:15 +0530

    feat: Implement environment-specific API URL configuration, update initial user data, and refresh the application favicon.

--- project-resource-roster/src/app/service/user.service.ts
+++ project-resource-roster/src/app/service/user.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { User } from '../models/user';
+
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient) { }
-  private apiUrl = 'http://localhost:3000/users';
+  private apiUrl = `${environment.apiUrl}/users`;
 
   getUser(): Observable<User[]> {
     return this.http.get<User[]>(this.apiUrl)
